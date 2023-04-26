25d496859089e48c2aded7af6339dbc50e33a3f4
memojl <memojl08@gmail.com>
2023-04-26 08:41:11 -0600

    Update routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,6 +6,6 @@ router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
-    path("api/v1", include(router.urls))
+    path("api/v1/", include(router.urls))
 ]
 
