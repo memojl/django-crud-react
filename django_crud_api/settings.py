63b6edef80222a19713f8c671e75789e2268b04b
memojl <memojl08@gmail.com>
2023-04-26 22:08:32 -0600

    Update Docs API

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Cors
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
