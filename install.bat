14a17fd35e56535bdba725d0de0cf18c76fd2109
memojl <memojl08@gmail.com>
2023-05-04 11:03:04 -0600

    Update Sys

--- install.bat
+++ install.bat
@@ -11,12 +11,27 @@ CALL pip install django
 ECHO -
 CALL django-admin startproject django_crud_api .
 ECHO -
-:: PROBAR PROYECTO python manage.py runserver
-ECHO CONFIGURAR SETTINGS drframework/settings.py
-PAUSE
 CALL python manage.py startapp tasks
 ECHO -
+ECHO CONFIGURAR SETTINGS drframework/settings.py
+PAUSE
+CALL python manage.py migrate
+:: PROBAR PROYECTO python manage.py runserver
+ECHO -
 CALL pip install djangorestframework
+ECHO -
+CALL pip install django-cors-headers
+ECHO -
+ECHO CONFIGURAR SETTINGS drframework/settings.py
+PAUSE
+CALL python manage.py makemigrations
+ECHO -
+CALL python manage.py migrate
+ECHO -
+PAUSE
+CALL python manage.py createsuperuser
+PAUSE
+CALL pip install coreapi
 ECHO -
 
 :Bye
